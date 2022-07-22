5f38c7c4fda94d2d377b56d2d136498932c28650
Angga Irham Stianto <anggairham08@gmail.com>
2022-07-22 14:24:04 +0700

    update auto route, controller youtube

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,11 @@ class Home extends BaseController
 {
     public function index()
     {
+        // print_r($this->router->routes);
         return view('welcome_message');
+    }
+    public function test()
+    {
+        echo "string";
     }
 }
